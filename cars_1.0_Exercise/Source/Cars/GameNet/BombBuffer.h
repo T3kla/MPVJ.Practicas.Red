2cd190a28017bb0a66eb85475ba9b3141aac3d42
T3kla <manuillo94@gmail.com>
2022-04-04 20:01:10 +0200

    a

--- cars_1.0_Exercise/Source/Cars/GameNet/BombBuffer.h
+++ cars_1.0_Exercise/Source/Cars/GameNet/BombBuffer.h
@@ -1,18 +1,18 @@
-// #pragma once
+#pragma once
 
-// #include "Net/buffer.h"
-// #include "Math/Vector.h"
-// #include "Math/Transform.h"
+#include "Net/buffer.h"
+#include "Math/Vector.h"
+#include "Math/Transform.h"
 
-// class CBombBuffer : public Net::CBuffer
-// {
-//   public:
-//     CBombBuffer(size_t initsize = 500, size_t delta = 100);
-//     ~CBombBuffer();
+class CBombBuffer : public Net::CBuffer
+{
+  public:
+    CBombBuffer(size_t initsize = 500, size_t delta = 100);
+    ~CBombBuffer();
 
-//     using Net::CBuffer::read;
-//     using Net::CBuffer::write;
+    using Net::CBuffer::read;
+    using Net::CBuffer::write;
 
-//     void write(const FVector &_data);
-//     void read(FVector &_data);
-// };
+    void write(const FVector &_data);
+    void read(FVector &_data);
+};
